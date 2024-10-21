12751028a9e802432d861a7ff7e43d614d0b8097
Prashant Gupta <prashantguptaofficial007@gmail.com>
2024-10-21 11:52:54 +0530

    Add more button functionality

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -28,7 +28,7 @@ const TopDoctors = () => {
                 ))
             }
         </div>
-        <button className='bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10'>More</button>
+        <button onClick={()=> {navigate('/doctors'); scrollTo(0,0)}} className='bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10'>More</button>
     </div>
   )
 }
