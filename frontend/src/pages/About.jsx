4184e1a3b5b5728d808e577b8ae16b26736ee877
Prashant Gupta <prashantguptaofficial007@gmail.com>
2024-10-22 16:20:03 +0530

    complete about us page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -23,6 +23,27 @@ const About = () => {
           </p>
         </div>
       </div>
+
+      <div className='text-xl my-4'>
+        <p>WHY <span  className='text-gray-700 font-semibold'>CHOOSE US</span> </p>
+      </div>
+
+      <div className='flex flex-col md:flex-row mb-20'>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>Efficiency:</b>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia, quaerat.</p>
+        </div>
+        
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>Convenience:</b>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, sequi.</p>
+        </div>
+
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>Personalization:</b>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore, amet!</p>
+        </div>
+      </div>
     </div>
   )
 }
