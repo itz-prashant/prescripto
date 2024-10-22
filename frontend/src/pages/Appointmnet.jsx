e3222823400fe4719d45f685640747bed35c590d
Prashant Gupta <prashantguptaofficial007@gmail.com>
2024-10-22 13:42:59 +0530

    Add related doctors section in appointment page

--- frontend/src/pages/Appointmnet.jsx
+++ frontend/src/pages/Appointmnet.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import { AppContext } from '../context/AppContext';
 import { assets } from '../assets/assets';
+import RelatedDoctors from '../components/RelatedDoctors';
 
 const Appointmnet = () => {
 
@@ -124,7 +125,8 @@ const Appointmnet = () => {
         </div>
         <button className='bg-primary text-white text-sm font-light px-14 py-3 rounded-full my-6 mt-4'>Book an appointment</button>
       </div>
-
+        {/* -------- Listing related Doctors --------- */}
+        <RelatedDoctors docId={docId} speciality={docInfo.speciality}/>
     </div>
   )
 }
