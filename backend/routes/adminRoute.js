d0564371891cd006c6780bbdb719cb1aac092d08
Prashant Gupta <prashantguptaofficial007@gmail.com>
2024-10-28 19:21:05 +0530

    Add doctor list

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -2,11 +2,13 @@ import express from 'express'
 import { addDoctor, alldoctors, loginAdmin } from '../controllers/adminController.js'
 import upload from '../middlewares/multer.js'
 import authAdmin from '../middlewares/authAdmin.js'
+import { changeAvailability } from '../controllers/doctorController.js'
 
 const adminRouter = express.Router()
 
 adminRouter.post('/add-doctor', authAdmin ,upload.single('image'), addDoctor)
 adminRouter.post('/login', loginAdmin)
-adminRouter.get('/all-doctors',authAdmin, alldoctors)
+adminRouter.post('/all-doctors',authAdmin, alldoctors)
+adminRouter.post('/change-availablity',authAdmin, changeAvailability)
 
 export default adminRouter;
