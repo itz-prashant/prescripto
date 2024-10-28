ef8b7bdef096dc883a6893ebb22e2c19bada860f
Prashant Gupta <prashantguptaofficial007@gmail.com>
2024-10-28 17:12:26 +0530

    Add get all doctors api

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -108,4 +108,22 @@ const loginAdmin = async(req, res) =>{
     }
 }
 
-export {addDoctor, loginAdmin}
+// Api to get all doctor list for admin panel
+
+const alldoctors = async (req, res) =>{
+    try {
+        const doctors = await doctorModel.find({}).select('-password')
+        res.json({
+            success: true,
+            doctors
+        })
+    } catch (error) {
+        console.log(error);
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+export {addDoctor, loginAdmin, alldoctors}
