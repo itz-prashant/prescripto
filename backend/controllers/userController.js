88376c85502e42c9e9a39f5d36dedb85cafea7fa
Prashant Gupta <prashantguptaofficial007@gmail.com>
2024-10-29 18:07:39 +0530

    Add login controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -59,4 +59,44 @@ const registerUser = async (req, res)=>{
     }
 }
 
-export {registerUser}
+// Login user
+
+const loginUser = async(req, res)=>{
+
+    try {
+        const {email, password} = req.body
+        
+        const user = await userModel.find({email})
+
+        if(!user){
+            return res.json({
+                success: false,
+                message: 'User Does not exist'
+            })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+        
+        if(isMatch){
+            const token = jwt.sign({id:user._id}, process.env.JWT_SECRET)
+                res.json({
+                success: true,
+                token
+            })
+        }else{
+            res.json({
+                success:false,
+                message: 'Invalid credentials'
+            })
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+export {registerUser, loginUser}
