444e809316807f94b09c790abde860adc0182bba
Prashant Gupta <prashantguptaofficial007@gmail.com>
2024-10-29 16:02:53 +0530

    Display all doctors on frontend from database

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,10 +11,13 @@ import Appointment from './pages/Appointmnet'
 import NotFound from './pages/Not-found'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer} from 'react-toastify';
 
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
+      <ToastContainer />
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,16 +1,38 @@
-import { createContext } from "react";
-import { doctors } from "../assets/assets";
+import { createContext, useEffect, useState } from "react";
+import axios from 'axios'
+import {toast} from 'react-toastify'
 
 export const AppContext = createContext()
 
 const AppContextProvider = (props)=>{
 
+    const [doctors, setDoctors] = useState([])
+
     const currencySynbol = '$'
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
+
+    const getDoctorsData = async ()=>{
+        try {
+            const {data} = await axios.get(backendUrl +'/api/doctor/list')
+            if(data.success){
+                setDoctors(data.doctors)
+            }else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+    }
 
     const value = {
         doctors, currencySynbol
     }
     
+    useEffect(()=>{
+        getDoctorsData()
+    },[])
+
     return (
         <AppContext.Provider value={value}>
             {props.children}
