c1776aafff97d085b266f341c7742c703d6de734
Prashant Gupta <prashantguptaofficial007@gmail.com>
2024-11-09 16:24:51 +0530

    fetch all appointment from backend

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -8,6 +8,7 @@ const AdminContextProvider = (props) =>{
 
     const [aToken, setAToken] = useState(localStorage.getItem('aToken')?localStorage.getItem('aToken'): '')
     const [doctors, setDoctors] = useState([])
+    const [appointments, setAppointments] = useState([])
 
     const backendUrl = import.meta.env.VITE_BACKEND_URL
 
@@ -41,8 +42,22 @@ const AdminContextProvider = (props) =>{
         }
     }
 
+    const getAllAppointments = async ()=>{
+        try {
+            const{data} = await axios.get(backendUrl+'/api/admin/appointments', {headers: {aToken}})
+            if(data.success){
+                setAppointments(data.appointments)
+            }else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message)
+        }
+    }
+
     const value = {
-        aToken, setAToken, backendUrl, doctors, getAllDoctors, changeAvailablity
+        aToken, setAToken, backendUrl, doctors, getAllDoctors, changeAvailablity, getAllAppointments,setAppointments,appointments
     }
 
     return (
