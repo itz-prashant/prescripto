eb8ae7bac7fd7050b0295744d4586c5153e2a202
Prashant Gupta <prashantguptaofficial007@gmail.com>
2024-11-09 15:20:26 +0530

    add get appointment controller for admin panel

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -3,6 +3,7 @@ import bcrypt from 'bcrypt'
 import {v2 as cloudinary} from 'cloudinary'
 import doctorModel from '../models/doctorModel.js'
 import jwt from 'jsonwebtoken'
+import appointmentModel from '../models/appointmentModel.js'
 
 // Api fro adding doctors
 
@@ -126,4 +127,21 @@ const alldoctors = async (req, res) =>{
     }
 }
 
-export {addDoctor, loginAdmin, alldoctors}
+// Api to get all appointment list
+const appointmentAdmin = async (req,res)=>{
+    try {
+        const appointments = await appointmentModel.find({})
+        res.json({
+            success: true,
+            appointments
+        })
+    } catch (error) {
+        console.log(error);
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+export {addDoctor, loginAdmin, alldoctors, appointmentAdmin}
