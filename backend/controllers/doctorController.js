08220ed35d4a2338ff330bffd2f928b2ddef5381
Prashant Gupta <prashantguptaofficial007@gmail.com>
2024-11-11 17:20:32 +0530

    add login doctor controller

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -1,4 +1,6 @@
 import doctorModel from "../models/doctorModel.js"
+import  bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 const changeAvailability = async (req, res)=>{
 
@@ -37,4 +39,40 @@ const doctorList = async (req, res)=>{
 
 }
 
-export {changeAvailability, doctorList}
+// Api for doctor login
+const loginDoctor = async(req,res)=>{
+    try {
+        const {email, password} = req.body
+        const doctor = await doctorModel.findOne({email})
+
+        if(!doctor){
+            return res.json({
+                success: false,
+                message: 'Invalid Credentials'
+            })
+        }
+
+        const isMatch = await bcrypt.compare(password, doctor.password)
+
+        if(isMatch){
+            const token = jwt.sign({id:doctor._id}, process.env.JWT_SECRET)
+            return res.json({
+                success: true,
+                token
+            })
+        }else{
+            return res.json({
+                success: false,
+                message: error.message
+            })
+        }
+
+    } catch (error) {
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+export {changeAvailability, doctorList,loginDoctor}
