e45f2f91485a48ec00c815800d81fac217dcc6ac
Prashant Gupta <prashantguptaofficial007@gmail.com>
2024-11-11 12:29:57 +0530

    Add admin dashboard controller

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -4,6 +4,7 @@ import {v2 as cloudinary} from 'cloudinary'
 import doctorModel from '../models/doctorModel.js'
 import jwt from 'jsonwebtoken'
 import appointmentModel from '../models/appointmentModel.js'
+import userModel from '../models/userModel.js'
 
 // Api fro adding doctors
 
@@ -180,6 +181,32 @@ const appointmentCancel = async (req, res) => {
       });
     }
   };
+
+// Api to get dashboard data for admin panel
+const adminDashboard = async (req,res)=>{
+    try{
+        const doctors = await doctorModel.find({})
+        const users = await userModel.find({})
+        const appointments = await appointmentModel.find({}) 
+
+        const dashData = {
+            doctors: doctors.lenght,
+            appointments: appointments.length,
+            patients: users.length,
+            latestAppointment: appointments.reverse().slice(0,5)
+        }
+        res.json({
+            success:true,
+            dashData
+        })
+    }catch (error) {
+      console.log(error);
+      res.json({
+        success: false,
+        message: error.message,
+      });
+    }
+}
   
 
-export {addDoctor, loginAdmin, alldoctors, appointmentAdmin, appointmentCancel}
+export {addDoctor, loginAdmin, alldoctors, appointmentAdmin, appointmentCancel,adminDashboard}
