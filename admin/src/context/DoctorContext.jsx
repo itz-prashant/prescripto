5ea6de5ebf36f59cb0f53f4b48dbb53449a76436
Prashant Gupta <prashantguptaofficial007@gmail.com>
2024-11-15 19:04:32 +0530

    fetch doctor profile in doctor context

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -11,6 +11,8 @@ const DoctorContextProvider = (props) =>{
     const [dToken, setDToken] = useState(localStorage.getItem('dToken') ? localStorage.getItem('dToken') : '')
     const [appointments, setAppointments] = useState([])
     const [dashData, setDashData] = useState(false)
+    const [profileData, setProfileData] = useState(false)
+
 
     const getAppointment = async()=>{
         try {
@@ -73,8 +75,22 @@ const DoctorContextProvider = (props) =>{
         }
     }
 
+    const getProfileData = async ()=>{
+        try {
+            const {data} = await axios.get(backendUrl+'/api/doctor/profile', {headers:{dToken}})
+            if(data.success){
+                setProfileData(data.profileData)
+                console.log(data.profileData);
+                
+            }
+        } catch (error) {
+            console.log(error.message);
+            toast.error(error.message)
+        }
+    }
+
     const value = {
-        dToken, setDToken, backendUrl, getAppointment, appointments, setAppointments, completeAppointment,dashData,setDashData,getDashData, cancelAppointment
+        dToken, setDToken, profileData, setProfileData, getProfileData, backendUrl, getAppointment, appointments, setAppointments, completeAppointment,dashData,setDashData,getDashData, cancelAppointment
     }
 
     return (
